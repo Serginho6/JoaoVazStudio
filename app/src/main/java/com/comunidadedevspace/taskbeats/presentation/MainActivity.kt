f4426359524cdae8eca442454b5f4a94ae751272
Sérgio Bismark <sergiobismark06@gmail.com>
2023-05-11 18:48:01 -0300

    Implement FloatActionButton and Fragments

--- app/src/main/java/com/comunidadedevspace/taskbeats/presentation/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/MainActivity.kt
@@ -2,11 +2,40 @@ package com.comunidadedevspace.taskbeats.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import com.comunidadedevspace.taskbeats.R
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottom_nav_view)
+
+        val taskListFragment = TaskListFragment.newInstance()
+        val newsListFragment = NewsListFragment.newInstance()
+
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container_view, taskListFragment)
+            setReorderingAllowed(true)
+        }
+
+        bottomNavView.setOnItemSelectedListener { menuItem ->
+            val fragment = when(menuItem.itemId){
+                R.id.task_list -> taskListFragment
+                R.id.news_list -> newsListFragment
+                else -> taskListFragment
+            }
+
+            supportFragmentManager.commit {
+                replace(R.id.fragment_container_view, fragment)
+                setReorderingAllowed(true)
+            }
+
+            true
+
+        }
     }
 }
---
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/NewsListFragment.kt
@@ -0,0 +1,29 @@
+package com.comunidadedevspace.taskbeats.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.comunidadedevspace.taskbeats.R
+
+class NewsListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_news_list, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         * @return A new instance of fragment NewsListFragment.
+         */
+        @JvmStatic
+        fun newInstance() = NewsListFragment()
+    }
+}
