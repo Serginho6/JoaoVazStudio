5ba18a9c1a0f8fd356a31352b07da71b0d604b9a
Sergio Bismark Cim Marchewsky <sergiobismark06@gmail.com>
2023-09-20 15:43:26 -0300

    Made "User" Parcelable to receive data on EditProfile card soon, and added "UserDao" Update.

--- app/src/main/java/com/comunidadedevspace/joaovazstudio/data/User.kt
+++ app/src/main/java/com/comunidadedevspace/joaovazstudio/data/User.kt
@@ -1,12 +1,14 @@
 package com.comunidadedevspace.joaovazstudio.data
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "users")
 data class User(
     @PrimaryKey(autoGenerate = true)
-    val id: Long = 0,
+    var id: Long = 0,
     val name: String,
     val email: String,
     val password: String,
@@ -14,4 +16,39 @@ data class User(
     val gender: String,
     val height: String,
     val weight: String
-)
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readLong(),
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: "",
+        parcel.readString() ?: ""
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeLong(id)
+        parcel.writeString(name)
+        parcel.writeString(email)
+        parcel.writeString(password)
+        parcel.writeString(phone)
+        parcel.writeString(gender)
+        parcel.writeString(height)
+        parcel.writeString(weight)
+    }
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<User> {
+        override fun createFromParcel(parcel: Parcel): User {
+            return User(parcel)
+        }
+
+        override fun newArray(size: Int): Array<User?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/com/comunidadedevspace/joaovazstudio/data/UserDao.kt
+++ app/src/main/java/com/comunidadedevspace/joaovazstudio/data/UserDao.kt
@@ -4,11 +4,16 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface UserDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(user: User)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(user: User)
+
     @Query("SELECT * FROM users WHERE email = :email")
     suspend fun getUserByEmail(email: String): User?
 }
