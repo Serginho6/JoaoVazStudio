6cab35138b83a8fcff55fc0c20233e381084ccc4
Sergio Bismark Cim Marchewsky <sergiobismark06@gmail.com>
2023-10-02 14:31:24 -0300

    Changed FloatActionButton position.

--- app/src/main/java/com/comunidadedevspace/joaovazstudio/presentation/TrainSelectedFragment.kt
+++ app/src/main/java/com/comunidadedevspace/joaovazstudio/presentation/TrainSelectedFragment.kt
@@ -9,58 +9,12 @@ import com.comunidadedevspace.joaovazstudio.R
 
 class TrainSelectedFragment : Fragment() {
 
-//    private lateinit var selectedTrainContent: LinearLayout
-//
-//    //Adapter
-//    private val exerciseAdapter: ExerciseListAdapter by lazy {
-//        ExerciseListAdapter(requireContext(), ::openExerciseListDetail)
-//    }
-//
-//    private val exerciseViewModel: ExerciseListViewModel by lazy {
-//        ExerciseListViewModel.create(requireActivity().application, getCurrentUserId())
-//    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_selected_train, container, false)
     }
-
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        val rvTasks: RecyclerView = view.findViewById(R.id.rv_exercise_list)
-//        rvTasks.adapter = exerciseAdapter
-//
-//        exerciseAdapter.setOnItemClickListener { task ->
-//            val videoId = task.youtubeVideoId
-//            if (!videoId.isNullOrEmpty()) {
-//                val editTextVideoId = requireActivity().findViewById<EditText>(R.id.edt_task_video_url)
-//                editTextVideoId?.setText(videoId)
-//            }
-//        }
-//    }
-//
-//    override fun onStart() {
-//        super.onStart()
-//        listFromDataBase()
-//    }
-//
-//    private fun listFromDataBase() {
-//        //Observer
-//        val listObserver = Observer<List<Exercise>>{ listTasks ->
-//            exerciseAdapter.submitList(listTasks)
-//        }
-//
-//        //Live data
-//        exerciseViewModel.exerciseListLiveData.observe(this, listObserver)
-//    }
-//
-//    private fun openExerciseListDetail(exercise: Exercise) {
-//        val intent = ExerciseDetailActivity.start(requireContext(), exercise, 0)
-//        requireActivity().startActivity(intent)
-//    }
 
     companion object {
         @JvmStatic
